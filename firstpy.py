109834e0f11383c988f23bfdb6eb12220d8acd03
Thulani Mafalala <thulanism@gmail.com>
2024-11-26 21:13:22 +0200

    Added basic calculator

--- firstpy.py
+++ firstpy.py
@@ -9,3 +9,25 @@ print("Variable x = " + str(x))
 # Re-using the value of a variable
 x = x*10
 print("x is now = " +str(x))
+
+# A basic calculator
+
+# Get first value
+a = int(input("Enter the first number: "))
+
+# Get the operator (+ - * /)
+op = input("Indicate operation + - * /: ")
+
+# Get second value
+b = int(input("Enter the second number: "))
+
+if op == "+":
+    c = a+b
+elif op == "-":
+    c = a-b
+elif op == "*":
+    c = a*b
+else:
+    c = a/b
+
+print(int(c))
