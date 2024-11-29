20dd5c983d9b1df8629894ece3c485fd8b54477d
Thulani Mafalala <thulanism@gmail.com>
2024-11-29 22:19:13 +0200

    Using func to calculate

---
+++ calc_with_funcs.py
@@ -0,0 +1,35 @@
+# A Calculator With Functions
+
+# Get first value
+def get_first_value():
+    a = int(input("Enter the first number: "))
+    return a
+
+# Get the operator (+ - * /)
+def get_operator():
+    op = input("Indicate operation + - * /: ")
+    return op
+
+# Get second value
+def get_second_value():
+    b = int(input("Enter the second number: "))
+    return b
+
+# Calculate Answer
+
+v1 = get_first_value()
+vo = get_operator()
+v2 = get_second_value()
+
+if vo == "+":
+    result = v1+v2
+elif op == "-":
+    result = v1-v2
+elif op == "*":
+    result = v1*v2
+elif op == "/":
+    result = v1/v2
+else:
+    result = "Invalid Operator!"
+
+print(result)
