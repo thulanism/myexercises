9ce8a937fe3e20a054802c5f95e73bbf29ddce9a
Thulani Mafalala <thulanism@gmail.com>
2025-01-09 20:28:57 +0200

    Updated timer

--- timer.py
+++ timer.py
@@ -2,14 +2,16 @@ import time
 import os
 
 def countdown(time_sec):
+    os.system('clear') 
+
     while time_sec:
-        os.system('clear')
         mins, secs = divmod(time_sec, 60)
         timeformat = '{:02d}:{:02d}'.format(mins, secs)
         print(timeformat, end='\r')
         time.sleep(1)
         time_sec -= 1
+        os.system('clear') 
 
     print("stop")
 
-countdown(120)
+countdown(40)
